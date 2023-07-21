345686ea2dae12548cb49476bc74eefc435f309c
Akash Kumar <akashkumar59999@gmail.com>
2023-07-21 13:30:33 +0530

    completed with changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Items from "./components/Items";
 function App() {
   const [items, setItems] = useState([]);
   const [editItem, setEditItem] = useState(null);
-  const [itemCount,setItemCount] = useState(0);
-  const [completeCount,setCompleteCount] = useState(0);
+  const [itemCount, setItemCount] = useState(0);
+  const [completeCount, setCompleteCount] = useState(0);
 
   function handleClose(id) {
     const updatedItems = items.filter((item) => item.id !== id);
@@ -15,8 +15,10 @@ function App() {
     setItemCount(updatedItems.length);
     setCompleteCount(updatedItems.length);
 
-    const completedItems = updatedItems.filter((item)=> item.status === 'completed');
-    setCompleteCount(completedItems.length)
+    const completedItems = updatedItems.filter(
+      (item) => item.status === "completed"
+    );
+    setCompleteCount(completedItems.length);
   }
 
   function handleEditItem(item) {
@@ -34,18 +36,18 @@ function App() {
   function handleUp(item) {
     const index = items.findIndex((e) => e.id === item.id);
     const newItems = [...items];
-    if(index !== 0){
-      newItems.splice(index,1);
-      newItems.splice(index-1,0,item);
+    if (index !== 0) {
+      newItems.splice(index, 1);
+      newItems.splice(index - 1, 0, item);
       setItems(newItems);
     }
   }
   function handleDown(item) {
     const index = items.findIndex((e) => e.id === item.id);
     const newItems = [...items];
-    if(index !== (newItems.length - 1)){
-      newItems.splice(index,1);
-      newItems.splice(index+1,0,item);
+    if (index !== newItems.length - 1) {
+      newItems.splice(index, 1);
+      newItems.splice(index + 1, 0, item);
       setItems(newItems);
     }
   }
@@ -53,30 +55,35 @@ function App() {
     setItems([...items, { ...item, id: Math.floor(Math.random() * 100 + 1) }]);
     setItemCount(items.length + 1);
   }
-  function handleComplete(item){
+  function handleComplete(item) {
     const index = items.findIndex((e) => e.id === item.id);
     let newItem = {};
-    if(item.status === 'completed'){
-      newItem = {...item,status:'not-completed'}
-    }else{
-      newItem = {...item,status:'completed'}
+    if (item.status === "completed") {
+      newItem = { ...item, status: "not-completed" };
+    } else {
+      newItem = { ...item, status: "completed" };
     }
     const newItems = [...items];
-    newItems.splice(index,1,newItem);
+    newItems.splice(index, 1, newItem);
     setItems(newItems);
-  
-    const completedItems = newItems.filter((item)=> item.status === 'completed');
-    setCompleteCount(completedItems.length)
+
+    const completedItems = newItems.filter(
+      (item) => item.status === "completed"
+    );
+    setCompleteCount(completedItems.length);
   }
   return (
     <div className="App">
+      <h5>Build by Akash Kumar</h5>
       <h1>Todo - List</h1>
       <Form
         onSubmit={handleSubmit}
         editItem={editItem}
         onEdit={handleEditItem}
       />
-      <h2>Completed Items: {completeCount} / {itemCount}</h2>
+      <h2>
+        Completed Items: {completeCount} / {itemCount}
+      </h2>
       <Items
         items={items}
         onComplete={handleComplete}
